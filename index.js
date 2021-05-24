e71974dccbd62522d5cac82adfb145995b7fc646
Mimi Vo <mimzy414@gmail.com>
2021-05-23 20:49:42 -0500

    updated 'os' to 'fs'

--- index.js
+++ index.js
@@ -1,35 +1,56 @@
 // Packages needed for this application
 const inquirer = require('inquirer');
-const { type } = require('os');
+const fs = require('fs');
+
 const Employee = require('./lib/Employee');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern')
+const Manager = require('./lib/Manager')
+
+// APPLICATION FUNCTION
 
 const initApp = () => {
-    inquirer
-        .prompt([
-            {
-                type: "text",
-                name: "name",
-                message: "What is your name?"
-            },
-            {
-                type: "number",
-                name: "id",
-                message: "What is your ID?",
-                validate: (number) => {
-                    if (typeof number === "number") {
-                        return number;
-                    }
+    inquirer.prompt([
+        {
+            type: "text",
+            name: "name",
+            message: "What is your name?"
+        },
+        {
+            type: "number",
+            name: "id",
+            message: "What is your ID?",
+            validate: (number) => {
+                if (typeof number === "number") {
+                    return number;
                 }
-            },
-            {
-                type: "text",
-                name: "email",
-                message: "What is your email address?",
-            },
-        ])
-        .then((responses) => console.log(responses));
+            }
+        },
+        {
+            type: "text",
+            name: "email",
+            message: "What is your email address?",
+        },
+    ])
+    .then((responses) => console.log(responses));
         console.log(new Employee(response.name, response.id, response.email));
  };
 
-// Function call to initialize app
+
+ // GET MANAGER DATA FUNCTION
+
+
+ //GET TEAM MEMBER DATA FUNCTION
+
+
+ // ADD ENGINEER 
+
+ // ADD INTERN
+
+
+
+ //CREATE HTML FUNCTION WITH ALL TEAM MEMBERS
+
+
+// FUNCTION TO KICKOFF APP
 initApp();
