baf0e47c0e14160ca622fe5cc3bb8c50e5525965
Mimi Vo <mimzy414@gmail.com>
2021-05-23 21:50:07 -0500

    removed officenumber function

--- index.js
+++ index.js
@@ -1,51 +1,58 @@
 // Packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const path = require('path')
 
 const Employee = require('./lib/Employee');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern')
 const Manager = require('./lib/Manager')
 
+// const teamMembers = [];
+
 // APPLICATION FUNCTION
 
-const initApp = () => {
-    inquirer.prompt([
-        {
-            type: "text",
-            name: "name",
-            message: "What is your name?"
-        },
-        {
-            type: "number",
-            name: "id",
-            message: "What is your ID?",
-            validate: (number) => {
-                if (typeof number === "number") {
-                    return number;
-                }
-            }
-        },
-        {
-            type: "text",
-            name: "email",
-            message: "What is your email address?",
-        },
-    ])
-    .then((responses) => console.log(responses));
-        console.log(new Employee(response.name, response.id, response.email));
- };
+    // WELCOME MESSAGE
+    function initApp() {
+        inquirer.prompt([
+            {
+                type: "input",
+                message: "Hello fellow coder! Ready to put together your team profile?",
+            } 
+        ]) 
+            addManager();
+        };
+
+    // GET MANAGER DATA FUNCTION
+    function addManager() {
+        inquirer.prompt([
+            {
+                type: "input",
+                message: "What is your team manager's name?",
+                name: "name"
+            },
+            {
+                type: "input",
+                message: "Please enter your team manager's email.",
+                name: "email"
+            },
+            {
+                type: "input",
+                message: "Please enter your team manager's office number?",
+                name: "officeNumber"
+            },
+        ])
+
+            addTeamMembers();
+    }
+        
+
+    //GET TEAM MEMBER DATA FUNCTION
 
 
- // GET MANAGER DATA FUNCTION
+    // ADD ENGINEER 
 
-
- //GET TEAM MEMBER DATA FUNCTION
-
-
- // ADD ENGINEER 
-
- // ADD INTERN
+    // ADD INTERN
 
 
 
