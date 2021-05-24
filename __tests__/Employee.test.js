ee52d9bae43c56a1c318833cb47b593d685405d8
Mimi Vo <mimzy414@gmail.com>
2021-05-23 19:43:45 -0500

    created adn passed Manager tests

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -2,7 +2,7 @@ const Employee = require('../lib/Employee')
 
 test('Get name input from constructor argument', () => {
     const testInputEl = 'Mimi'
-    const testNewEmployee = new Employee(testInputEl, 1234, "mimzy414@gmail.com");
+    const testNewEmployee = new Employee(testInputEl, 1234, 'mimzy414@gmail.com');
 
     expect(testNewEmployee.name).toBe(testInputEl);
 }); 
@@ -15,36 +15,36 @@ test('Get email input from constructor argument', () => {
 }); 
 
 test('Get id input from constructor argument', () => {
-    const testInputEl = '123'
-    const testNewEmployee = new Employee('Mimi', testInputEl, "mimzy414@gmail.com");
+    const testInputEl = 1234
+    const testNewEmployee = new Employee('Mimi', testInputEl, 'mimzy414@gmail.com');
 
     expect(testNewEmployee.id).toBe(testInputEl);
 }); 
 
 test('getName() returns name', () => {
     const testInputEl = 'Employee';
-    const testNewEmployee = new Employee(testInputEl, 1234, "mimzy414@gmail.com");
+    const testNewEmployee = new Employee(testInputEl, 1234, 'mimzy414@gmail.com');
 
     expect(testNewEmployee.getName()).toBe(testInputEl);
 }); 
 
 test('getId() returns id number', () => {
     const testInputEl = '1234';
-    const testNewEmployee = new Employee(testInputEl, testInputEl, "mimzy414@gmail.com");
+    const testNewEmployee = new Employee('Mimi', testInputEl, 'mimzy414@gmail.com');
 
     expect(testNewEmployee.getId()).toBe(testInputEl);
 }); 
 
 test('getEmail() returns email', () => {
     const testInputEl = 'mimzy414@gmail.com';
-    const testNewEmployee = new Employee(testInputEl, 1234, testInputEl);
+    const testNewEmployee = new Employee('Mimi', 1234, testInputEl);
 
     expect(testNewEmployee.getEmail()).toBe(testInputEl);
 });
 
 test('\'Employee\' is returned when getRole() is called', () => {
     const testRole = 'Employee';
-    const testNewEmployee = new Employee("Mimi", 1234, "mimzy414@gmail.com", testRole);
+    const testNewEmployee = new Employee('Mimi', 1234, 'mimzy414@gmail.com', testRole);
 
     expect(testNewEmployee.getRole()).toBe(testRole);
 }); 
--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,1 +1,25 @@
-const Manager = require('../lib/Manager.js');
+const Manager = require('../lib/Manager');
+
+test('Creates a new Manager object', () => {
+    const testNewManager = new Manager('Mimi', 1234, 'mimzy414@gmail.com', 1234);
+
+    expect(testNewManager.name).toEqual(expect.any(String));
+    expect(testNewManager.email).toEqual(expect.any(String));
+    expect(testNewManager.id).toEqual(expect.any(Number));
+    expect(testNewManager.officeNumber).toEqual(expect.any(Number));
+});
+
+test('officeNumber() returns office number', () => {
+    const testInputEl= 1234;
+    const testNewManager = new Manager("Mimi", 1234, "mimzy414@gmail.com", testInputEl);
+    console.log(testInputEl);
+    expect(testNewManager.getOfficeNumber()).toBe(testInputEl);
+});
+
+test('\'Manager\' is returned when getRole() is called', () => {
+    const testInputEl = 'Manager';
+    const testNewManager = new Manager("Mimi", 1234, "mimzy414@gmail.com", testInputEl);
+    console.log(testInputEl);
+
+    expect(testNewManager.getRole()).toBe(testInputEl);
+});
